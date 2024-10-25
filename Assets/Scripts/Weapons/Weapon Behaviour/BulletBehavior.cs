27f417c1009e2bedd9ea5f05deae53509f03b492
Joao.Freitas <jvitorfreitas2004@gmail.com>
2024-10-25 01:14:18 -0300

    feature/map-design

--- Assets/Scripts/Weapons/Weapon Behaviour/BulletBehavior.cs
+++ Assets/Scripts/Weapons/Weapon Behaviour/BulletBehavior.cs
@@ -14,7 +14,7 @@ public class BulletBehaviour : ProjectileWeaponBehaviour
     void Update()
     {
         // Move a bala para a direção desejada
-        transform.position += moveDirection * weaponData.Speed * Time.deltaTime;    
+        transform.position += moveDirection * weaponData.Speed * Time.deltaTime;
     }
 
     public void DirectionChecker(Vector3 direction)
@@ -23,18 +23,24 @@ public class BulletBehaviour : ProjectileWeaponBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-{
-    // Verifica se colidiu com o jogador
-    if (collision.gameObject.CompareTag("Player"))
     {
-        collision.gameObject.GetComponent<PlayerStats>()?.TakeDamage(weaponData.Damage);
-        Destroy(gameObject); // Destruir a bala ao colidir com o jogador
+        // Verifica se colidiu com o jogador
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerStats>()?.TakeDamage(weaponData.Damage);
+            Destroy(gameObject); // Destruir a bala ao colidir com o jogador
+        }
+
+        // Ignorar colisão com inimigos
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+
+        // Destruir a bala ao colidir com o Tilemap Collider 2D
+        if (collision.gameObject.CompareTag("Tilemap"))
+        {
+            Destroy(gameObject); // Destruir a bala ao colidir com o Tilemap
+        }
     }
-    
-    // Ignorar colisão com inimigos
-    if (collision.gameObject.CompareTag("Enemy"))
-    {
-        Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
-    }
-}
 }
